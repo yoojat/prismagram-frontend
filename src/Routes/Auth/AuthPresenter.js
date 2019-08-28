500ac84ebd05dd57e7effbfa2be0a82c6f04b34f
yoojat <yooja.ty@gmail.com>
2019-08-28 15:13:16 +0900

    confirmSecret + LogIn Mutation

--- src/Routes/Auth/AuthPresenter.js
+++ src/Routes/Auth/AuthPresenter.js
@@ -81,18 +81,20 @@ export default ({
       )}
       )}
     </Form>
-    <StateChanger>
-      {action === "logIn" ? (
-        <>
-          Don't have an account?{" "}
-          <Link onClick={() => setAction("signUp")}>Sign up</Link>
-        </>
-      ) : (
-        <>
-          Have an account?{" "}
-          <Link onClick={() => setAction("logIn")}>Log in</Link>
-        </>
-      )}
-    </StateChanger>
+    {action !== "confirm" && (
+      <StateChanger>
+        {action === "logIn" ? (
+          <>
+            Don't have an account?{" "}
+            <Link onClick={() => setAction("signUp")}>Sign up</Link>
+          </>
+        ) : (
+          <>
+            Have an account?{" "}
+            <Link onClick={() => setAction("logIn")}>Log in</Link>
+          </>
+        )}
+      </StateChanger>
+    )}
   </Wrapper>
 );
