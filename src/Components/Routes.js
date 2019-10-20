e4b2605aed79ef8b882969246701fa6341e08bcf
yoojat <yooja.ty@gmail.com>
2019-10-20 23:35:51 +0900

    #9.2 Log Out and Conclusions

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
 import Explore from "../Routes/Explore";
@@ -14,6 +14,7 @@ const LoggedInRoutes = () => (
       <Route path="/explore" component={Explore} />
       <Route path="/search" component={Search} />
       <Route path="/:username" component={Profile} />
+      <Redirect from="*" to="/" />
     </Switch>
   </>
 );
@@ -21,6 +22,7 @@ const LoggedInRoutes = () => (
 const LoggedOutRoutes = () => (
   <Switch>
     <Route exact path="/" component={Auth} />
+    <Redirect from="*" to="/" />
   </Switch>
 );
 
